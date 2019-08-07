f108fb72f282b565c2740fbfe82311ef08111b7f
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-08-07 18:00:56 +0300

    Packaging...

--- setup.py
+++ setup.py
@@ -6,13 +6,23 @@ setuptools.setup(
     author="L. Kärkkäinen",
     author_email="tronic@noreply.users.github.com",
     description="DNS server with DNS-over-HTTPS and Redis caching",
-    long_description="A caching DNS server that uses Google and Cloudflare DNS-over-HTTPS and answers queries on UDP port 53 super-fast.",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Tronic/named1",
     packages=setuptools.find_packages(),
-    classifiers=[
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = [
+        "trio>=0.12",
+        "trio-redis",
+        "h2",
+        "dnspython",
+    ],
+    dependency_links = [
+        "git+https://github.com/Tronic/trio-redis.git#egg=trio-redis",
+    ],
+    include_package_data = True,
 )
